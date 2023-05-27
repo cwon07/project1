c9763fe5e355f81306dba636cd583be8a6931374
Christine Wong <jumiryoo@gmail.com>
2023-05-27 01:47:14 -0700

    Updated HTML, CSS, and JS. API data now appears on the app but only one brewery at a time.

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
-window.addEventListener("load", function(){
-    setTimeout(
-        function open(event) {
-            document.querySelector(".popup").style.display = "block";
-        },
-        1000
-    )
-});
+//////// pop up window set up ////////
 
-document.querySelector("#close").addEventListener("click", function(){
-    document.querySelector(".popup").style.display = "none";
-});
+// window.addEventListener("load", function(){
+//     setTimeout(
+//         function open(event) {
+//             document.querySelector(".popup").style.display = "block";
+//         },
+//         1000
+//     )
+// });
+
+// document.querySelector("#close").addEventListener("click", function(){
+//     document.querySelector(".popup").style.display = "none";
+// });
+
+
+//////// main app set up //////////
 
 const $searchForm = $("form");
 
@@ -19,53 +24,42 @@ $searchForm.on("submit", event => {
     const formData = new FormData(event.target);
     // generate data from the target object
     console.log(formData);
-    // get the value from the generated data where the namevalue is "pokemon" (on the form)
+    // get the value from the generated data where the name value is "breweries" (on the form)
     // toLowerCase allows items to be searched regardless of alphabet casing
     const breweries = formData.get("breweries").toLowerCase();
     console.log(formData.get('breweries'));
     
-    const url = `https://api.openbrewerydb.org/v1/breweries/search?query={search}`
+    const url = `https://api.openbrewerydb.org/v1/breweries/?by_city=${breweries}`
+
+    console.log(url)
+
+    const $mainResult = $(".mainResult")
+
+    $('[name="breweries"]')[0].value = "";
 
     ////// fetch using AJAX ///////
     $.ajax(url)
         .then((data) => {
-            console.log(data);
-            console.log(data.filter((item, index) => {
-                console.log(index, item);
-                console.log({name: item.name, city: item.city, state: item.state, type: item.brewery_type, website: item.website_url})
+            console.log(data.filter((item) => {
+        $mainResult.html(
+            `<div>
+                <b>Name:&nbsp; </b> ${item.name}
+            </div>
+            <div>
+                <b>Brewery Type:&nbsp; </b> ${item.brewery_type}
+            <div>
+                <b>City:&nbsp; </b> ${item.city}
+            </div>
+            <div>
+                <b>State:&nbsp; </b> ${item.state}
+            <div>
+                <b>Website:&nbsp; </b> ${item.website_url}
+            `
+            )
             }))
         })
-    ///// fetch using JSON ///////
-//     const $result = $(".result")
+        .catch(() => {
+            $mainResult.html(`<div> Unable to find the city...</div>`)
+        })
 
-//     // empty out the input field
-//     $('[name="breweries"]')[0].value = "";
-//     $result.html('<div>Loading...</div>') // loading comment 
-
-//     fetch(url)
-//         .then(response => {
-//             return response.json();
-//         })
-//         .then(data => {
-//             $result.html(
-//                 `<div>
-//                     <b>name:&nbsp; </b> ${data.name}
-//                 </div>
-//                 <div>
-//                     <b>brewery_type :&nbsp; </b> ${data.brewery_type}
-//                 </div>
-//                 <div>
-//                     <b>city:&nbsp; </b> ${data.city}
-//                 </div>
-//                 <div>
-//                     <b>state:&nbsp; </b> ${data.state}
-//                 </div>
-//                 <div>
-//                     <b>website_url:&nbsp; </b> ${data.website_url}
-//                 </div>
-//             `)
-//         })
-//         .catch(() => {
-//             $result.html(`<div> there was an error...</div>`)
-//         })
-});
+    });
